7b56d148987ff28e493b4cfdbffe57e4329e7d87
Mike Freeman <mfreema5@hawk.iit.edu>
2015-04-24 16:36:47 -0500

    Get GitHub JSON for last 4 users.

--- grab-gh-uid.sh
+++ grab-gh-uid.sh
@@ -2,7 +2,8 @@
 
 touch gh-users.json
 
-for FOO in karlstolley profstolley avelazq1  huesitos  jhignight  mfreema5  nsoparaw  sicueft  thedanmartin  YishanHuang
+#for FOO in karlstolley profstolley avelazq1  huesitos  jhignight  mfreema5  nsoparaw  sicueft  thedanmartin  YishanHuang
+for FOO in sburnet1 kscom381zk jsilva4 dtoppel
 do
   curl "https://api.github.com/users/${FOO}"  >> gh-users.json
   echo "" >> gh-users.json
