49e0fd8ad2afa9b2c4dec13d242faaee4fce98c3
Mike Freeman <mfreema5@hawk.iit.edu>
2015-05-03 05:31:42 -0500

    Re-run ‘grab-gh-uid.sh’

--- grab-gh-uid.sh
+++ grab-gh-uid.sh
@@ -2,8 +2,7 @@
 
 touch gh-users.json
 
-#for FOO in karlstolley profstolley avelazq1  huesitos  jhignight  mfreema5  nsoparaw  sicueft  thedanmartin  YishanHuang
-for FOO in sburnet1 kscom381zk jsilva4 dtoppel
+for FOO in karlstolley profstolley avelazq1 dtoppel huesitos jhignight jsilva4 kscom381zk mfreema5 nsoparaw sburnet1 sicueft thedanmartin YishanHuang
 do
   curl "https://api.github.com/users/${FOO}"  >> gh-users.json
   echo "" >> gh-users.json
